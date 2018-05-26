d51eb87e4b0fc4cc8ce4c7f094d408abf58209e9
Or Nachmias <or.nachmias@gmail.com>
2018-05-26 22:37:47 +0300

    Additional infrastructure classes

---
+++ src/main/java/App/CommandLineInterface.java
@@ -0,0 +1,22 @@
+package App;
+
+import java.io.File;
+
+public class CommandLineParser {
+    public String getFile(String[] args){
+        if (args == null || args.length == 0)
+            return null;
+
+        if (!new File(args[0]).exists())
+            return null;
+
+        return args[0];
+    }
+
+    public void printHelp(){
+        System.out.println("No input file was detected.\n" +
+                "It is possible that no arguments were inserted " +
+                "or the file does not exists in the current directory.\n" +
+                "USAGE: java.exe -jar <JAR-FILE> <PARAMETERS-FILE>");
+    }
+}
